e8dd4c9524227b8a94100f0baa2c84fa97e6d2bd
gitanushka <77441008+gitanushka@users.noreply.github.com>
2024-02-24 19:26:41 +0530

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 104
+let accountName = "Anushka"
+accountEmail = "anu10@gmail.com"
+//even though no keyword before variable , memory allocated
+let accountCity
+//no value assigned thus it is termed as undefined
+var accountSalary = "2000000"
+// var is not prefered to be used due to issue in block scope and functionl scope
+
+console.table([accountId, accountName, accountEmail, accountCity, accountSalary]);
