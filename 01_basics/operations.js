3f8859f95d7f7326ce1111c016b22d577ad8abed
gitanushka <77441008+gitanushka@users.noreply.github.com>
2024-02-29 20:12:12 +0530

    refact : console

--- 01_basics/operations.js
+++ 01_basics/operations.js
@@ -2,9 +2,9 @@
 console.log(2+2)
 let value = 3
 let negvalue = -value
-cosnole.log(negvalue)
+console.log(negvalue)
 
 let str1 = "Anu"
 let str2 = "shka"
 let str3 = str1+str2
-cosnole.log(str3)
+console.log(str3)
