a5f6c32e69b338739bb3e3257b3dc3618590908f
gitanushka <77441008+gitanushka@users.noreply.github.com>
2024-02-29 22:31:55 +0530

    refact: comparison ===

--- 01_basics/operations.js
+++ 01_basics/operations.js
@@ -8,3 +8,23 @@ let str1 = "Anu"
 let str2 = "shka"
 let str3 = str1+str2
 console.log(str3)
+
+
+//comparison 
+
+console.log(2>1)
+console.log(2==1)
+//js automatically convert 2 string to number
+console.log("2">1)
+console.log("02" > 1)
+
+console.log(null > 0)
+console.log(null == 0)
+console.log(null>=0)
+
+console.log(undefined == 0)
+console.log(undefined > 0)
+console.log(undefined < 0)
+
+// === strictly check =>value as well as datatype
+console.log("2" === 2)
