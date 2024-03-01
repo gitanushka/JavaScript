28dd4695d8c3bd05d57608f50a20cfa48cdcec75
gitanushka <77441008+gitanushka@users.noreply.github.com>
2024-03-01 16:33:07 +0530

    refact : strings

---
+++ 01_basics/strings.js
@@ -0,0 +1,21 @@
+const name = "Anushka"
+const age = 21
+
+// the below syntax to concatenate string 
+// console.log(name + count + " Value");
+
+console.log(`Hello my name is ${name} and my age is ${age}`)
+
+//string can be declared in below given way
+
+const gameName = new String('anushkaquinn')
+console.log(gameName[0])
+//the value of below code look empty object but it isnt it consists of several object methods and functions
+console.log(gameName.__proto__)
+
+//accessing object methods
+console.log(gameName.length)
+console.log(gameName.toUpperCase())
+console.log(gameName.charAt(2))
+console.log(gameName.indexOf('q'))
+
